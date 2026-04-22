49322e6e6cdce9ac7c92a6b6350e56a9292e4603
chiranjeevisampathirao-boop <chiranjeevisampathirao@gmail.com>
2026-04-22 15:43:10 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("enter A value \n");
+    scanf("%d",&a);
+    printf("A is even number i.e is:%d",a%2==0);
+    return 0;
+}
