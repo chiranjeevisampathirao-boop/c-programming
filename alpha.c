82107c05686887ad4ecf760bd190daff96879487
chiranjeevisampathirao-boop <chiranjeevisampathirao@gmail.com>
2026-04-22 15:41:29 +0530

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("enter the character \n");
+    scanf("%c",&ch);
+    int res=(ch>='A')&&(ch<='Z')||(ch>='a')&&(ch<='z');
+    printf("%c is lower case alphabet i.e is:%d",ch,res); 
+A
+    return 0;
+}
